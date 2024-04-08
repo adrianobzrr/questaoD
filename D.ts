2c6aed35d6c34b4c8b550b00ef53cc6ff9fce22d
Adriano Pessoa <adrianobpessoa@gmail.com>
2024-04-08 11:34:38 -0300

    secound commit

--- D.ts
+++ D.ts
@@ -1,16 +1,16 @@
 const contaNumerosBonitos = (inicio: number, fim: number): number => {
-    let count = 0;
-    for(let i = inicio; i <= fim; i++){
-        if(temDigito(8, i) && temDigito(3, i)){
-            count++;
-        }
+  let count = 0;
+  for (let i = inicio; i <= fim; i++) {
+    if (temDigito(8, i) && !temDigito(3, i)) {
+      count++;
     }
-    return count;
-}
+  }
+  return count;
+};
 
 const temDigito = (digito: number, valor: number): boolean => {
-    return String(valor).includes(String(digito));
-}
+  return String(valor).includes(String(digito));
+};
 
 const inicio = 18907;
 const fim = 33306;
