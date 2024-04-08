2ac7093e477907774f36a1d8d4c6daa8392f30b1
Adriano Pessoa <adrianobpessoa@gmail.com>
2024-04-08 11:21:53 -0300

    first commit

---
+++ D.ts
@@ -0,0 +1,18 @@
+const contaNumerosBonitos = (inicio: number, fim: number): number => {
+    let count = 0;
+    for(let i = inicio; i <= fim; i++){
+        if(temDigito(8, i) && temDigito(3, i)){
+            count++;
+        }
+    }
+    return count;
+}
+
+const temDigito = (digito: number, valor: number): boolean => {
+    return String(valor).includes(String(digito));
+}
+
+const inicio = 18907;
+const fim = 33306;
+
+console.log(contaNumerosBonitos(inicio, fim));
